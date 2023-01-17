345e65b3c18eda144727cb3b960583bcdab25748
bbam <nunnapats1822@gmail.comr>
2023-01-17 16:23:22 +0700

    bblab15_1completed

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -3,7 +3,20 @@ using namespace std;
 
 template <typename T>
 void insertionSort(T d[],int N){
-
+    int i, j;
+    T z;
+    for(i = 1; i < N; i++){
+        z = d[i];
+        j = i-1;
+    while(j >=0 && d[j] < z){
+        d[j+1] = d[j];
+        j--;
+    }
+    d[j+1] = z;
+    cout << "Pass " << i << ":";
+    for(int i = 0; i < 10; i++) cout << d[i] << " ";
+    cout << "\n";
+}
 }
 
 int main(){
@@ -17,3 +30,4 @@ int main(){
 	cout << "\nSorted Array:";
 	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
+
